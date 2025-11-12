7f756146208622f73a75c7e2811cd112a5922a5d
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-11-12 23:44:24 +0100

    App

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -10,14 +10,14 @@ const Counter = () => {
     <div className="p-6 bg-white rounded-xl shadow-md text-center">
         <h1 className="text-xl font-semibold text-gray-700 mb-4">Compteur Interactif</h1>
         <p className="text-2xl font-bold text-blue-600">{count}</p>
-        <div className="flex justify-center gap-2">
-          <button onClick={decrement} className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700 transition">
+        <div>
+          <button onClick={() => setCount(count + 1)} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
             Decrementer
           </button>
-          <button onClick={reset} className="mt-4 px-4 py-2 bg-gray-600 text-white rounded-md hover:bg-gray-700 transition">
+          <button onClick={() => setCount(count + 1)} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
           Reinitialiser
         </button>
-        <button onClick={increment} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+        <button onClick={() => setCount(count + 1)} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
           Incrementer
         </button>
         </div>

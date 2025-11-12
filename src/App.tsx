c804ff666d57071b35d70b6b6f77dcd1c0959274
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-11-12 23:23:10 +0100

    Counter 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Counter from './components/Counter'
 function App() {
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100'>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-gray-200'>
       <h1 className='text-3xl font-bold mb-6 text-gray-800'>Gerer les etats avec React</h1>
       <Counter />
     </div>
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>Counter</div>
+    <div className="p-6 bg-white rounded-xl shadow-md text-center">
+        <h1 className="text-xl font-semibold text-gray-700 mb-4">Compteur Interactif</h1>
+        <p className="text-2xl font-bold text-blue-600">{count}</p>
+        <button onClick={() => setCount(count + 1)} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+          Incrementer
+        </button>
+    </div>
   )
 }
 
